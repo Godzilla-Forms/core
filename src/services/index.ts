400636b786f55ffd7221b56ac8d3678c4334acbe
alhazmy13 <me@alhazmy13.net>
2022-10-22 23:08:41 +0300

    fix lint

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,7 +1,6 @@
 import { GodzillaFormCombinedValues } from '../model';
 
-export abstract class GodzillaService {
-}
+export abstract class GodzillaService {}
 
 export abstract class GodzillaDataService extends GodzillaService {
   abstract getData(): Promise<GodzillaFormCombinedValues[]>;
