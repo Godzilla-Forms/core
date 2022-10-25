5ba91bfe9293b87bacd3e0051ec91e970b38897b
alhazmy13 <me@alhazmy13.net>
2022-10-26 02:58:59 +0300

    fix code format and eslint

--- src/model/godzilla.model.ts
+++ src/model/godzilla.model.ts
@@ -1,102 +1,105 @@
 import {
-  GodzillaConditions, GodzillaFormType, GodzillaItemTypes, GodzillaValueSource,
+	GodzillaConditions,
+	GodzillaFormType,
+	GodzillaItemTypes,
+	GodzillaValueSource,
 } from './godzilla.enum';
 
 export interface GodzillaFormCombinedValues {
-  label: string;
-  value: string;
+	label: string;
+	value: string;
 }
 
 export interface GodzillaForm {
-  id?: string;
-  title?: string;
-  style: GodzillaFormStyle;
-  pages: GodzillaFormPage[];
+	id?: string;
+	title?: string;
+	style: GodzillaFormStyle;
+	pages: GodzillaFormPage[];
 }
 
 export interface GodzillaFormStyle {
-  type: GodzillaFormType;
-  showSubmitButton?: boolean;
-  submitLabel?: string;
-  nextLabel?: string;
-  previousLabel?: string;
+	type: GodzillaFormType;
+	showSubmitButton?: boolean;
+	submitLabel?: string;
+	nextLabel?: string;
+	previousLabel?: string;
 }
 
 export interface GodzillaFormPage {
-  id: string;
-  title: string;
-  controls: GodzillaFormControls[];
+	id: string;
+	title: string;
+	controls: GodzillaFormControls[];
 }
 
 export interface GodzillaFormControls {
-  id: string;
-  label?: string;
-  placeholder?: string;
-  subheader?: string;
-  value: GodzillaFormControlValues;
-  type: GodzillaItemTypes;
-  options?: GodzillaFormControlOptions;
-  file?: GodzillaFileControl;
-  style?: GodzillaFormControlStyle;
-  validators?: GodzillaFormControlValidator;
-  errors?: GodzillaFormControlErrors;
-  flow?: GodzillaFormControlFlow;
+	id: string;
+	label?: string;
+	placeholder?: string;
+	subheader?: string;
+	value: GodzillaFormControlValues;
+	type: GodzillaItemTypes;
+	options?: GodzillaFormControlOptions;
+	file?: GodzillaFileControl;
+	style?: GodzillaFormControlStyle;
+	validators?: GodzillaFormControlValidator;
+	errors?: GodzillaFormControlErrors;
+	flow?: GodzillaFormControlFlow;
 }
 
 export interface GodzillaFormControlFlow {
-  basedOn?: string;
-  value?: string;
-  condition?: GodzillaConditions;
+	basedOn?: string;
+	value?: string;
+	condition?: GodzillaConditions;
 }
 
 export interface GodzillaFileControl {
-  multiple: boolean;
+	multiple: boolean;
 }
 
 export interface GodzillaFormControlValues {
-  defaultValue: string | boolean;
-  valueOptions?: GodzillaFormCombinedValues[];
-  serviceName?: string;
-  valueSource?: GodzillaValueSource;
+	defaultValue: string | boolean;
+	valueOptions?: GodzillaFormCombinedValues[];
+	serviceName?: string;
+	valueSource?: GodzillaValueSource;
 }
 
 export interface GodzillaFormControlOptions {
-  hidden?: boolean;
-  disabled?: boolean;
+	hidden?: boolean;
+	disabled?: boolean;
 }
 
 export interface GodzillaFormControlStyle {
-  floating?: boolean;
-  col?: number;
-  colXs?: number;
-  colSm?: number;
-  colMd?: number;
-  colLg?: number;
-  colXl?: number;
-  colXxl?: number;
-  customCss?: string;
+	floating?: boolean;
+	col?: number;
+	colXs?: number;
+	colSm?: number;
+	colMd?: number;
+	colLg?: number;
+	colXl?: number;
+	colXxl?: number;
+	customCss?: string;
 }
 
 export const defaultStyleValues: GodzillaFormControlStyle = {
-  col: 12,
+	col: 12,
 };
 
 export interface GodzillaFormControlValidator {
-  pattern?: string;
-  required?: boolean;
-  min?: number;
-  max?: number;
-  minLength?: number;
-  maxLength?: number;
-  maxSize?: number;
-  allowedTypes?: string;
+	pattern?: string;
+	required?: boolean;
+	min?: number;
+	max?: number;
+	minLength?: number;
+	maxLength?: number;
+	maxSize?: number;
+	allowedTypes?: string;
 }
 
 export interface GodzillaFormControlErrors {
-  pattern?: string;
-  required?: string;
-  min?: string;
-  max?: string;
-  minLength?: string;
-  maxLength?: string;
+	pattern?: string;
+	required?: string;
+	min?: string;
+	max?: string;
+	minLength?: string;
+	maxLength?: string;
 }
